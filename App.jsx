e0c3851fc79c08c2a2ddc01ded2cfef4033b6273
intellicode12 <khushimokani1207@gmail.com>
2026-04-07 17:36:05 +0530

    Fix missing newline at end of App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+    </>
+  );
+}
